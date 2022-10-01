7dff88c78c5fce06d4343af93a546da0b31da73f
Ross <rosswbillington@gmail.com>
2022-10-01 00:11:14 -0400

    Create Operand functions to store runner
    Created functions to take in the value of the button pressed for both numbers and the operands

--- javascript.js
+++ javascript.js
@@ -1,5 +1,8 @@
-function add(a,b) {
-    
+var runner=0;
+let digit;
+
+function add(runner) {
+    console.log(runner);
     //take input from which button
     //is clicked on screen
 
@@ -10,7 +13,37 @@ document.querySelectorAll('button').forEach(el =>{
         digit_pressed(el.textContent);
     });
 });
-    function digit_pressed(digit) {
-        console.log("digit pressed: " + digit);
-        return digit;
+function digit_pressed(digit) {
+    console.log("digit pressed: " + digit);
+    getRunner(digit);
+}
+function getRunner(digit) {
+    if (String(digit) === "+") {
+        console.log('+: ' + digit)
+    //these will be operand functions
+    }  
+    else if (String(digit) === "-") {
+        console.log('-: ' + digit)
+    //these will be operand functions
     }
+    else if (String(digit) === "*") {
+        console.log('*: ' + digit)
+    //these will be operand functions
+    }
+    else if (String(digit) === "/") {
+        console.log('/: ' + digit)
+    //these will be operand functions
+    }    
+    else if (typeof Number(digit) === 'number') {
+        runner = Number(digit);
+        console.log('number runner: ' + runner);
+        return runner;
+    } 
+    // else if (digit === "/") {
+    //     console.log('+: ' + digit)
+    // //these will be operand functions
+    // }    
+}
+function test() {
+    console.log('runner' + runner);
+}
