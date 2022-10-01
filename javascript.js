a820cecf2c7d706e040caf4c8733c00d6d4fd676
Ross <rosswbillington@gmail.com>
2022-10-01 12:01:21 -0400

    Remove digit_pressed(), Update display to value of button pressed

--- javascript.js
+++ javascript.js
@@ -2,48 +2,55 @@ var runner=0;
 let digit;
 
 function add(runner) {
-    console.log(runner);
+    console.log(runner + secondVal)
+    console.log("f")
+
     //take input from which button
     //is clicked on screen
 
     //add the numbers
 }
-document.querySelectorAll('button').forEach(el =>{
+
+//Add event listeners to numeral buttons
+document.querySelectorAll('button.num').forEach(el =>{
     el.addEventListener('click', () =>{
         digit_pressed(el.textContent);
     });
 });
-function digit_pressed(digit) {
-    console.log("digit pressed: " + digit);
-    getRunner(digit);
-}
+
 function getRunner(digit) {
-    if (String(digit) === "+") {
-        console.log('+: ' + digit)
+    if (typeof Number(digit) === 'number') {
+        runner = Number(digit);
+    }  
+    document.getElementById("display-results").innerHTML = runner;
+    return runner;
+
+
+}
+// Add eventListeners to operand buttons
+document.querySelectorAll('button.operand').forEach(el =>{
+    el.addEventListener('click', () =>{
+        useOperand(el.textContent);
+    });
+});
+function useOperand(operand) {
+    if (String(operand) === "+") {
+        runner = String(operand);
+        // runner=operand;
+        // add();
     //these will be operand functions
     }  
-    else if (String(digit) === "-") {
-        console.log('-: ' + digit)
+    else if (String(operand) === "-") {
+        runner = String(operand);
     //these will be operand functions
     }
-    else if (String(digit) === "*") {
-        console.log('*: ' + digit)
+    else if (String(operand) === "*") {
+        runner = String(operand);
     //these will be operand functions
     }
-    else if (String(digit) === "/") {
-        console.log('/: ' + digit)
+    else if (String(operand) === "/") {
+        runner = String(operand);
     //these will be operand functions
-    }    
-    else if (typeof Number(digit) === 'number') {
-        runner = Number(digit);
-        console.log('number runner: ' + runner);
-        return runner;
-    } 
-    // else if (digit === "/") {
-    //     console.log('+: ' + digit)
-    // //these will be operand functions
-    // }    
-}
-function test() {
-    console.log('runner' + runner);
+    }
+    document.getElementById("display-results").innerHTML = runner;
 }
