aa6ae99eb98bc657f785d5f97fc7e1b04ecd3917
Ross <rosswbillington@gmail.com>
2022-09-30 23:34:55 -0400

    Create function to return digit/operand when button pressed

--- javascript.js
+++ javascript.js
@@ -1,7 +1,16 @@
 function add(a,b) {
-    return a+b;
+    
     //take input from which button
     //is clicked on screen
 
     //add the numbers
 }
+document.querySelectorAll('button').forEach(el =>{
+    el.addEventListener('click', () =>{
+        digit_pressed(el.textContent);
+    });
+});
+    function digit_pressed(digit) {
+        console.log("digit pressed: " + digit);
+        return digit;
+    }
