f64e520ac3a86793f3bc73efd3fd0f8e02f34fc7
Ross <rosswbillington@gmail.com>
2022-10-01 23:10:32 -0400

    Display multiple numbers in display

--- javascript.js
+++ javascript.js
@@ -7,7 +7,23 @@ const equalEl = document.querySelector('.equal');
 const clearEl = document.querySelector('.all-clear');
 const clearLastEl = document.querySelector('.last-entity-clear');
 
+let dis1Num = '';
+let dis2Num = '';
+let result = null;
+let lastOperation = '';
+let haveDot = false;
 
+numbersEl.forEach(number => {
+    number.addEventListener('click', (e)=> {
+        if (e.target.innerText === '.' && !haveDot) {
+            haveDot = true;
+        } else if (e.target.innerText === '.' && haveDot) {
+            return;
+        }
+        dis2Num += e.target.innerText;
+        display2El.innerHTML = dis2Num;
+    })
+})
 
 // var runner;
 // let digit;
